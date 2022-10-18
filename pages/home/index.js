c9f3ad2913c791b59a6789e6be578756bc1142ad
Joao Borchoski <joaohenriqueborchoski>
2022-10-17 21:56:15 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,94 @@
-/* Desenvolva sua lógica aqui...*/
+//https://api.github.com
+let dataUsers = []
+let dataUrlUser = ""
+
+if(jsonAnalysis()){
+    dataUsers = jsonAnalysis()
+    //console.log(dataUsers)
+}
+
+function butSearchUser(){
+    //const hrefUser = document.getElementById("hrefUser")
+    const butUserHome = document.getElementById("butUserHome")
+    const inputUserHome = document.getElementById("inputUserHome")
+    const secUserNotFound = document.getElementById("secUserNotFound")
+    butUserHome.addEventListener("click", async (event) => {
+        event.preventDefault()
+        const data = await fetch(`https://api.github.com/users/${inputUserHome.value}`, {
+            method: "GET",
+            })
+            .then(res => res.json())
+            .then(res => {
+                //console.log(res)
+                if(res.message != 'Not Found'){
+                    secUserNotFound.innerHTML = ""
+                    dataUrlUser = ""
+                    dataUrlUser = `https://api.github.com/users/${inputUserHome.value}`
+                    const searchJson = JSON.stringify(dataUsers)
+                    localStorage.setItem("users", searchJson)
+                    if(verifyJson(jsonAnalysis("users")) != res.login || jsonAnalysis().length == 0){
+                        dataUsers.push(res)
+                    }
+
+                    localStorage.removeItem("user")
+                    const userLocarStorage = JSON.stringify(res)
+                    localStorage.setItem("user", userLocarStorage)
+
+                    //window.location.replace("http://127.0.0.1:5500/index-profile.html")
+                    //setTimeout(criaPerfil(jsonAnalysis("user")), 1000)
+                }else{
+                    console.log("erro")
+                    secUserNotFound.innerHTML = ""
+                    secUserNotFound.insertAdjacentHTML("beforeend", `
+                        <p>Usuário não encontrado</p>
+                    `)
+                }
+                //console.log(dataUrlUser)
+                //console.log(dataUsers)
+                
+                return res
+            })
+            .catch(err => console.log(err))
+            return data
+        
+    })
+}
+butSearchUser()
+
+function imgUserUpdater(arr){
+    let imgUsers = document.querySelectorAll(".imgUser")
+    const arrFor = arr.forEach(element => {
+        imgUsers.forEach(img => {
+            if(img.src != element.avatar_url){
+                img.src = element.avatar_url
+            }
+        })
+    })
+}
+imgUserUpdater(jsonAnalysis("users"))
+
+function jsonAnalysis(key){
+    const jsonLocalStorage = localStorage.getItem(key)
+    if(jsonLocalStorage){
+        const localStorageObj = JSON.parse(jsonLocalStorage)
+
+        
+        //console.log(localStorage)
+        return localStorageObj
+    }
+    
+}
+//console.log(jsonAnalysis("users"))
+
+function verifyJson(arr){
+    if(arr){
+        const verify = arr.forEach(element => element.login)
+    }
+}
+//verifyJson(jsonAnalysis())
+
+
+
+
+
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,81 @@
-/* Desenvolva sua lógica aqui...*/
+//https://api.github.com/users/Borchoski
+const cardProfile = document.querySelector(".cardProfile")
+
+
+function criaPerfil(element){
+    const mainProfile = document.getElementById("mainProfile")
+    //mainProfile.innerHTML = ""
+    cardProfile.innerHTML = ""
+    let section1 = document.createElement("section")
+    let img = document.createElement("img")
+    let section2 = document.createElement("section")
+    let h1 = document.createElement("h1")
+    let p = document.createElement("p")
+
+    img.src = element.avatar_url
+    h1.innerText = element.name
+    p.innerText = element.bio
+
+    section2.classList = ("sec2 flex column justifyCenter gap8")
+    section1.classList.add("sec1")
+
+    section1.appendChild(img)
+    section2.append(h1, p)
+
+    cardProfile.append(section1, section2)
+   
+}
+setTimeout(criaPerfil(jsonAnalysis("user")), 1000)
+
+async function criaRepos(url){
+    const ulRepos = document.getElementById("ulRepos")
+    const data = await fetch(url, {
+            method: "GET",
+            })
+            .then(res => res.json())
+            .then(res => {
+                const repos = res.forEach(element => {
+
+                    let li = document.createElement("li")
+                    let h2 = document.createElement("h2")
+                    let p = document.createElement("p")
+                    let section = document.createElement("section")
+                    let a = document.createElement("a")
+                    let button1 = document.createElement("button")
+                    let button2 = document.createElement("button")
+                    
+                
+                    h2.innerText = element.name
+                    p.innerText = element.description
+                    if(!element.description){
+                        p.innerText = "No description."
+                    }
+                    button1.innerText = "Repositório"
+                    button2.innerText = "Demo"
+                    a.href = element.html_url
+                    a.target = "_blank"
+
+                
+                    li.classList = ("card flex column gap32")
+                    section.classList = ("flex gap16")
+                    button1.classList.add("butRep")
+                    button2.classList.add("butDemo")
+                
+                    section.append(a, button2)
+                    a.appendChild(button1)
+                    li.append(h2, p, section)
+                    ulRepos.appendChild(li)
+                })
+            })
+            return data
+}
+criaRepos(reposUrl(jsonAnalysis("user")))
+//console.log(reposUrl(jsonAnalysis("user")))
+
+function reposUrl(obj){
+    const urlRepos = obj.repos_url
+    //console.log(urlRepos)
+
+    return urlRepos
+}
+
